7e14051038d3a94832bb0e58a289dcf9b23e4418
sparksThefire <darkdraman@gmail.com>
2015-03-05 21:17:01 -0600

    added walls, platform, and jumping need to work on camera movement around player

--- First Project/Assets/Movement/CameraController.cs
+++ First Project/Assets/Movement/CameraController.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+    private Vector3 offset;
+
 	public GameObject player;
-	private Vector3 offset;
+    public float cameraSensitivity;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position;
+        cameraSensitivity = 4.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = player.transform.position + offset;
+
+        //float angleIntensity = Input.GetAxis("Horizontal");
+
+        //transform.LookAt(player.transform);
+        //transform.Rotate(Vector3.up * angleIntensity * cameraSensitivity, Time.deltaTime);
+
+        //transform.Rotate(player.transform.position, angleIntensity * cameraSensitivity);
 	}
 }
--- First Project/Assets/Movement/PlayerMovement.cs
+++ First Project/Assets/Movement/PlayerMovement.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private const float ZERO = 0.0f;
+
 	private Rigidbody rigidBody;
 	private bool canJump;
+    private bool jumpPeak;
 
 	public float speedScale;
+    public float jumpMomentum;
 	public Vector3 maxVelocity;
 	public KeyCode jumpKey;
 
@@ -14,10 +18,12 @@ public class PlayerMovement : MonoBehaviour {
 		//Private
 		rigidBody = GetComponent<Rigidbody> ();
 		canJump = false;
+        jumpPeak = false;
 
 		//Public
 		speedScale = 10.0f;
-		maxVelocity = new Vector3 (0.5f, 0.75f, 0.5f);
+        jumpMomentum = 10.0f;
+		maxVelocity = new Vector3 (0.5f, 20.0f, 0.5f);
 		jumpKey = KeyCode.Space;
 	}
 
@@ -27,39 +33,62 @@ public class PlayerMovement : MonoBehaviour {
 		//Gets 'wasd' input
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		float moveJump = 0.0f;
+		float moveJump = ZERO;
 
 		if (canJump && Input.GetKey (jumpKey)) {
-			moveJump = 10.0f;
+            moveJump = jumpMomentum;
 			canJump = false;
 		}
 
-		Vector3 movement = new Vector3 (moveHorizontal, moveJump, moveVertical);
+		Vector3 movement = new Vector3 (ZERO, moveJump, moveVertical);
+        Vector3 rotate = new Vector3();
 
 		//Control speed/height
 		if (Mathf.Abs(movement.x) > maxVelocity.x) {
 			float negative = 1.0f;
-			if (movement.x < 0.0f) { negative *= -1; }
+            if (movement.x < ZERO) { negative *= -1; }
 			movement.x = maxVelocity.x * negative;
 		}
 		if (Mathf.Abs(movement.z) > maxVelocity.z) {
 			float negative = 1.0f;
-			if (movement.z < 0.0f) { negative *= -1; }
+            if (movement.z < ZERO) { negative *= -1; }
 			movement.z = maxVelocity.z * negative;
 		}
 		if (Mathf.Abs(movement.y) > maxVelocity.y) {
 			float negative = 1.0f;
-			if (movement.y < 0.0f) { negative *= -1; }
+            if (movement.y < ZERO) { negative *= -1; }
 			movement.y = maxVelocity.y * negative;
 		}
+
 
 		//Apply movement
 		rigidBody.AddForce (movement * speedScale);
 
-		if (rigidBody.position.y <= .01) {
-			canJump = true;
-		}
+//		if (rigidBody.position.y <= .01) {
+//			canJump = true;
+//		}
+
+        if (rigidBody.velocity.y < ZERO)
+        {
+            jumpPeak = true;
+        }
+
+        if (jumpPeak)
+        {
+            if (rigidBody.velocity.y >= ZERO)
+            {
+                canJump = true;
+                jumpPeak = false;
+            }
+        }
 		
 		Debug.Log ("Movement: " + movement + " Maximum velocity: " + maxVelocity);
 	}
+
+//	void OnCollisionEnter(collision : Collision) {
+//		if (rigidBody.grounded()) 
+//		{
+//			canJump = true;
+//		}
+//	}
 }
